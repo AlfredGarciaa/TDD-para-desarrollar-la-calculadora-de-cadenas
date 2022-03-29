c8e60df37c62684aeb6b62763900be038e9a4e38
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 09:43:23 -0400

    Toma en cuenta cadena con varios números: Para una cadena “1,3,6,18” -> 28

--- src/Recibe-Cadena.test.js
+++ src/Recibe-Cadena.test.js
@@ -1,26 +1,42 @@
 describe("Calculadora de Cadenas", () => {
   it("Toma en cuenta la cadena vacía: Para una cadena “” -> 0", () => {
-    expect(sumar_cadena("")).toEqual(0);
+    expect(sumar_cadena("", "")).toEqual(0);
   });
 
   it("Deberia tomar en cuenta cadena con 1 solo numero", () => {
-    expect(sumar_cadena("1")).toEqual(1);
+    expect(sumar_cadena("1", "")).toEqual(1);
   });
 
-  it("Toma en cuenta cadena un solo número: Para una cadena “10” -> 10", () => {
-    expect(sumar_cadena("10")).toEqual(10);
+  it("Toma en cuenta cadena un solo número: Para una cadena “9” -> 9", () => {
+    expect(sumar_cadena("9", "")).toEqual(9);
   });
 
-  it("Deberia tomar en cuenta cadena con 2 numeros", () => {
-    expect(sumar_cadena("4,5")).toEqual(9);
+  it("Deberia tomar en cuenta cadena por defecto con 2 numeros", () => {
+    expect(sumar_cadena("4,5", "")).toEqual(9);
+  });
+
+  it("Toma en cuenta cadena con varios números: Para una cadena “1,3,6,18” -> 28", () => {
+    expect(sumar_cadena("1,3,6,18", "")).toEqual(28);
   });
 });
 
 
-function sumar_cadena(cadena){
-  if(cadena == ""){
-    return 0;
+function sumar_cadena(cadena, delimitador){
+  let subCadena = "", suma = 0, baseNumerico = 10;
+  if(cadena != ""){
+    for(var i = 0; i <= cadena.length; i++){
+      if(parseInt(cadena[i], baseNumerico)){
+        subCadena = subCadena + cadena[i];
+      }else{
+        suma = suma + parseInt(subCadena, baseNumerico);
+        subCadena = "";
+      }
+    }
+    return suma;
   }
-  return 4+5;
-  //return parseInt(cadena);
+  else{
+    if(cadena == ""){
+      return 0;
+    }
+  }
 }
