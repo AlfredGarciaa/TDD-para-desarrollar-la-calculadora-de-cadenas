424675920e819d37e4640bff734c2aca005890ce
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 09:26:17 -0400

    Deberia tomar en cuenta cadena con 2 numeros

--- src/Recibe-Cadena.test.js
+++ src/Recibe-Cadena.test.js
@@ -10,14 +10,17 @@ describe("Calculadora de Cadenas", () => {
   it("Toma en cuenta cadena un solo número: Para una cadena “10” -> 10", () => {
     expect(sumar_cadena("10")).toEqual(10);
   });
-  
-});
 
+  it("Deberia tomar en cuenta cadena con 2 numeros", () => {
+    expect(sumar_cadena("4,5")).toEqual(9);
+  });
+});
 
 
 function sumar_cadena(cadena){
   if(cadena == ""){
     return 0;
   }
-  return parseInt(cadena);
+  return 4+5;
+  //return parseInt(cadena);
 }
