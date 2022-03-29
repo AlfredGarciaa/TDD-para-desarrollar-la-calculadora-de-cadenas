c19db8e1da63d9b189a25cc20c529da40777edd8
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 08:56:55 -0400

    Deberia tomar en cuenta cadena con 1 solo numero

--- src/Recibe-Cadena.test.js
+++ src/Recibe-Cadena.test.js
@@ -2,8 +2,17 @@ describe("Calculadora de Cadenas", () => {
   it("Toma en cuenta la cadena vacía: Para una cadena “” -> 0", () => {
     expect(sumar_cadena("")).toEqual(0);
   });
+
+  it("Deberia tomar en cuenta cadena con 1 solo numero", () => {
+    expect(sumar_cadena("1")).toEqual(1);
+  });
 });
 
+
+
 function sumar_cadena(cadena){
-  return 0;
+  if(cadena == ""){
+    return 0;
+  }
+  return parseInt(cadena);
 }
