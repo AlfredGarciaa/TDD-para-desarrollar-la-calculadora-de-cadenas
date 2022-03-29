c7aaf7566e6fb8d0629f6ed2487b613bba88912c
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 10:56:39 -0400

    Poner la funcion sumar_cadena en otra carpeta, verificando que tickee correctamente los anteriores testers

--- src/Recibe-Cadena.test.js
+++ src/Recibe-Cadena.test.js
@@ -1,3 +1,5 @@
+import sumar_cadena from "./Calcular-Cadena.js";
+
 describe("Calculadora de Cadenas", () => {
   it("Toma en cuenta la cadena vacía: Para una cadena “” -> 0", () => {
     expect(sumar_cadena("", "")).toEqual(0);
@@ -38,34 +40,8 @@ describe("Calculadora de Cadenas", () => {
   it("calcular suma de una cadena con un delimitador de cualquier longitud ", () => {
     expect(sumar_cadena("1***2***3", "***")).toEqual(6);
   });
-});
 
-function sumar_cadena(cadena, delimitador){
-  if(cadena == ""){
-    return 0;
-  }
-  else{
-    let suma = 0, baseNumerico = 10;
-    if(delimitador){
-      cadena = cadena.split(delimitador)
-      for(var i = 0; i < cadena.length; i++){
-        suma = suma + parseInt(cadena[i], baseNumerico);
-      }
-    } 
-  
-    if(!delimitador) {
-      let subCadena = "";
-      let entero;
-      for(var i = 0; i <= cadena.length; i++){
-        if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
-          subCadena = subCadena + cadena[i];
-        }else{
-          entero = parseInt(subCadena, baseNumerico);
-          if(entero <= 1000) suma = suma + entero;
-          subCadena = "";
-        }
-      }
-    }
-    return suma;
-  }
-}
+  it("calcular suma de una cadena con un delimitador de cualquier longitud ", () => {
+    expect(sumar_cadena("400*1*3", "*")).toEqual(404);
+  });
+});
