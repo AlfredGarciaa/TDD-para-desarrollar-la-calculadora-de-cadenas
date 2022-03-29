e73ef966eebc4b26e04a95ba8787a2d4229f52a2
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 08:46:21 -0400

    Toma en cuenta la cadena vacía: Para una cadena “” -> 0

---
+++ src/Recibe-Cadena.test.js
@@ -0,0 +1,9 @@
+describe("Calculadora de Cadenas", () => {
+  it("Toma en cuenta la cadena vacía: Para una cadena “” -> 0", () => {
+    expect(sumar_cadena("")).toEqual(0);
+  });
+});
+
+function sumar_cadena(cadena){
+  return 0;
+}
