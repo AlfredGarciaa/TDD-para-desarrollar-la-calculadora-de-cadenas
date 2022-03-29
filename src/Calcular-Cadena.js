8246f627e4238ca565da5646a1f33c9cea1a67cd
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-29 11:19:37 -0400

    Refactorizacion con JS v6 para mejor sintaxis, se crea en una clase toda la funcion sumar_cadena (añadiendo arrange y Assert-Verificacion, con su resultado)

--- src/Calcular-Cadena.js
+++ src/Calcular-Cadena.js
@@ -1,31 +1,32 @@
-function sumar_cadena(cadena, delimitador){
-    if(cadena == ""){
-      return 0;
-    }
-    else{
-      let suma = 0, baseNumerico = 10;
-      if(delimitador){
-        cadena = cadena.split(delimitador)
-        for(var i = 0; i < cadena.length; i++){
-          suma = suma + parseInt(cadena[i], baseNumerico);
+class Calculador{
+    sumar_cadena(cadena, delimitador){
+        if(cadena == ""){
+        return 0;
         }
-      } 
-    
-      if(!delimitador) {
-        let subCadena = "";
-        let entero;
-        for(var i = 0; i <= cadena.length; i++){
-          if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
-            subCadena = subCadena + cadena[i];
-          }else{
-            entero = parseInt(subCadena, baseNumerico);
-            if(entero <= 1000) suma = suma + entero;
-            subCadena = "";
-          }
+        else{
+        let suma = 0, baseNumerico = 10;
+        if(delimitador){
+            cadena = cadena.split(delimitador)
+            for(var i = 0; i < cadena.length; i++){
+            suma = suma + parseInt(cadena[i], baseNumerico);
+            }
+        } 
+        if(!delimitador) {
+            let subCadena = "";
+            let entero;
+            for(var i = 0; i <= cadena.length; i++){
+            if(parseInt(cadena[i], baseNumerico) || cadena[i] == 0){
+                subCadena = subCadena + cadena[i];
+            }else{
+                entero = parseInt(subCadena, baseNumerico);
+                if(entero <= 1000) suma = suma + entero;
+                subCadena = "";
+            }
+            }
         }
-      }
-      return suma;
+        return suma;
+        }
     }
-  }
+}
 
-  export default sumar_cadena;
+export default Calculador;
--- src/Recibe-Cadena.test.js
+++ src/Recibe-Cadena.test.js
@@ -1,47 +1,72 @@
-import sumar_cadena from "./Calcular-Cadena.js";
+import Calculador from "./Calcular-Cadena.js";
 
 describe("Calculadora de Cadenas", () => {
   it("Toma en cuenta la cadena vacía: Para una cadena “” -> 0", () => {
-    expect(sumar_cadena("", "")).toEqual(0);
+    //Arange
+    const cal = new Calculador();
+    //Act
+    const result = cal.sumar_cadena("", "");
+    //Assert
+    expect(result).toEqual(0);
   });
 
   it("Deberia tomar en cuenta cadena con 1 solo numero", () => {
-    expect(sumar_cadena("1", "")).toEqual(1);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("1", "");
+    expect(result).toEqual(1);
   });
 
   it("Toma en cuenta cadena un solo número: Para una cadena “9” -> 9", () => {
-    expect(sumar_cadena("9", "")).toEqual(9);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("9", "");
+    expect(result).toEqual(9);
   });
 
   it("Deberia tomar en cuenta cadena por defecto con 2 numeros", () => {
-    expect(sumar_cadena("4,5", "")).toEqual(9);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("4,5", "");
+    expect(result).toEqual(9);
   });
 
   it("Toma en cuenta cadena con varios números: Para una cadena “1,3,6,18” -> 28", () => {
-    expect(sumar_cadena("1,3,6,18", "")).toEqual(28);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("1,3,6,18", "");
+    expect(result).toEqual(28);
   });
 
   it("calcular suma para cualquier cadena", () => {
-    expect(sumar_cadena("1-2,3", "")).toEqual(6);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("1-2,3", "");
+    expect(result).toEqual(6);
   });
 
   it("calcular suma de una cadena con un delimitador por defecto", () => {
-    expect(sumar_cadena("6;7;4", ";")).toEqual(17);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("6;7;4", ";");
+    expect(result).toEqual(17);
   });
 
   it("calcular suma de una cadena con un delimitador por defecto ... con 1 numero mayor a 1000", () => {
-    expect(sumar_cadena("2, 1001", "")).toEqual(2);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("2, 1001", "");
+    expect(result).toEqual(2);
   });
 
   it("calcular suma de una cadena con un delimitador por defecto ... con 1 numero mayor a 1000", () => {
-    expect(sumar_cadena("700, 100000,5000", "")).toEqual(700);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("700, 100000,5000", "");
+    expect(result).toEqual(700);
   });
 
   it("calcular suma de una cadena con un delimitador de cualquier longitud ", () => {
-    expect(sumar_cadena("1***2***3", "***")).toEqual(6);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("1***2***3", "***");
+    expect(result).toEqual(6);
   });
 
   it("calcular suma de una cadena con un delimitador de cualquier longitud ", () => {
-    expect(sumar_cadena("400*1*3", "*")).toEqual(404);
+    const cal = new Calculador();
+    const result = cal.sumar_cadena("400*1*3", "*");
+    expect(result).toEqual(404);
   });
 });
